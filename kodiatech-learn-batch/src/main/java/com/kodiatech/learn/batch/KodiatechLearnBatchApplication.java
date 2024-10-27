98b77dc6e411c62dd628f495fd6838f5f3f659d7
traore <moussa.traoreinfo@gmail.com>
2024-10-27 02:10:19 +0200

    organiser parent

---
+++ kodiatech-learn-batch/src/main/java/com/kodiatech/learn/batch/KodiatechLearnBatchApplication.java
@@ -0,0 +1,13 @@
+package com.kodiatech.learn.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodiatechLearnBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodiatechLearnBatchApplication.class, args);
+	}
+
+}
---
+++ kodiatech-learn-batch/src/test/java/com/kodiatech/learn/batch/KodiatechLearnBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodiatech.learn.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodiatechLearnBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

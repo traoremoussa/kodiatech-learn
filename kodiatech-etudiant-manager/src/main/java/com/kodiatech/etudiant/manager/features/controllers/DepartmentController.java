7035d96326844a68376649276478205b66879709
traore <moussa.traoreinfo@gmail.com>
2024-12-01 17:17:43 +0100

    add list dep

--- kodiatech-etudiant-manager/src/main/java/com/kodiatech/etudiant/manager/features/controllers/DepartmentController.java
+++ kodiatech-etudiant-manager/src/main/java/com/kodiatech/etudiant/manager/features/controllers/DepartmentController.java
@@ -1,13 +1,31 @@
 package com.kodiatech.etudiant.manager.features.controllers;
 
+import com.kodiatech.etudiant.manager.features.model.Department;
+import com.kodiatech.etudiant.manager.features.model.Student;
+import com.kodiatech.etudiant.manager.features.service.IDepartmentService;
+import com.kodiatech.etudiant.manager.features.service.impl.DepartmentServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 /*Controller
 @ResponseBody*/
 //for rest
 @RequestMapping("api/department")
+@RequiredArgsConstructor
 public class DepartmentController {
+
+    private final IDepartmentService departmentService;
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Department>> findAllDepartement(){
+        return ResponseEntity.ok().body( departmentService. findAllDepartement());
+    }
 
 }
--- kodiatech-etudiant-manager/src/main/java/com/kodiatech/etudiant/manager/features/service/IDepartmentService.java
+++ kodiatech-etudiant-manager/src/main/java/com/kodiatech/etudiant/manager/features/service/IDepartmentService.java
@@ -1,4 +1,9 @@
 package com.kodiatech.etudiant.manager.features.service;
 
+import com.kodiatech.etudiant.manager.features.model.Department;
+
+import java.util.List;
+
 public interface IDepartmentService {
+    public List<Department> findAllDepartement();
 }
